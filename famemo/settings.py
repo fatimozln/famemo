d776dca1a92f9df9ce6f8722730f22ec8a52fa3c
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-08 13:37:59 +0330

    start project

--- famemo/settings.py
+++ famemo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8$z^n2w1q3pdyxszkecyxq2+uwlgg*7@@19#%qp!v*_+f(1fw-'
+SECRET_KEY = 'cjm$3=3x@cag$zite-f4%$4b5_*x_7z5-wwrl5y#m3zw#3*ubj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
