12f7f7b8f2450b40d5229002288e3719f247c7bf
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-08 16:41:27 +0330

    add templates to DIRS in TEMPATES

--- famemo/settings.py
+++ famemo/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'famemo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
