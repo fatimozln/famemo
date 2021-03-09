59c07a6ff6009b3700a6e3388f5aa23ba4655ed6
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-09 16:30:18 +0330

    add url for slug and assign name to slug ad articles page and app name

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "articles"
 urlpatterns = [
-    path('', views.articles_list),
+    path('', views.articles_list, name='list'),
+    path('<slug>', views.articles_details, name='slug'),
 ]
