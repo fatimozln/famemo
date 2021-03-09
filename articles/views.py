734f9798ad237d3b0c8089d1549c71d6d771b7c4
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-09 10:39:20 +0330

    add feature to display articles based on the latest time and Assign a name to use it in html codes

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 
 
 def articles_list(request):
-    return render(request, 'articles/articles_list.html')
+    articles = models.Article.objects.all().order_by('date')
+    return render(request, 'articles/articles_list.html', {'articles': articles})
