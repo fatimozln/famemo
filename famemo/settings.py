b480df4a8b2f480f47d9cf6055fc45a527217faa
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-09 15:15:04 +0330

    add dir for use static failes in root

--- famemo/settings.py
+++ famemo/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,3 +109,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'content'),
+)
