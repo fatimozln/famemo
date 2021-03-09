49b440253cbf86ab353620ef29fd9cb85668bb33
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-09 10:36:25 +0330

    add str function In order not to recognize the articles as an object and display them under the name of titles

--- articles/models.py
+++ articles/models.py
@@ -8,3 +8,6 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
