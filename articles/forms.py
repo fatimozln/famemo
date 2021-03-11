9401ac71aee642c1d56f65490bfa3cdb1848aece
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-12 01:40:37 +0330

    add this file and class to Specify the items received based on the model

---
+++ articles/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .import models
+
+
+class Addpost(forms.ModelForm):
+    class Meta:
+        model = models.Article
+        fields = ['title', 'slug', 'body', 'image']
