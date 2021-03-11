998aa9813f69cf4274875b7c2fe4d26fe512d469
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-11 16:13:50 +0330

    We added the login library, but because it was similar to our function name, we assigned another name to that library and performed the login operation.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as login_library
 
 
 def signup(request):
@@ -17,6 +18,8 @@ def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
+            user = form.get_user()
+            login_library(request, user)
             return redirect('articles:list')
     else:
         form = AuthenticationForm()
