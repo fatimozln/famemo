db16a445735300330937608f0231a7531dfeec3e
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-11 15:57:41 +0330

    add login function to login forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def signup(request):
@@ -12,3 +11,13 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
