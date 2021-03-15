a19051ce709092154472af8b9da288b1880b65aa
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-15 22:58:01 +0330

    Personalize the admin panel

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from . import models
+from .models import Article
 
 # Register your models here.
-admin.site.register(models.Article)
+
+
+class Blogadmin(admin.ModelAdmin):
+    fieldsets = [
+        ('title',                {'fields': ['title']}),
+        ('content information',                {'fields': ['body', 'author']}),
+        ('image',                {'fields': ['image']}),
+        ('like',                {'fields': ['likes']}),
+    ]
+    list_display = ['title', 'date']
+    serch_fields = ['title', 'content']
+
+
+admin.site.register(Article, Blogadmin)
