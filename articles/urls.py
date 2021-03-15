f27aa522ea7b4125d7cc781204d5fc0b3b0e7091
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-15 22:59:18 +0330

    add link and unlink url

--- articles/urls.py
+++ articles/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.articles_list, name='list'),
     path('add_post', views.add_post, name='add'),
     path('<slug>', views.articles_details, name='slug'),
+    path('like/<postid>', views.like, name='like'),
+    path('unlike/<postid>', views.unlike, name='unlike'),
 ]
