e4bc99a5ebfe5e8ed3ca4ade01704a9c6abad83c
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-05-06 12:41:40 +0430

    add status to active or deactive user and update chenge User to MyUser

--- profiles/models.py
+++ profiles/models.py
@@ -1,14 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
+from accounts.models import MyUser
 # Create your models here.
+
+
+STATUS_CHOICES = [
+    ('active', 'active'),
+    ('deactive', 'deactive'),
+]
 
 
 class Profile(models.Model):
     name = models.CharField(max_length=50)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
     bio = models.CharField(max_length=255)
     image = models.ImageField(
         default='default_profile.jpg', upload_to='%Y/%m/%d')
+    status = models.CharField(choices=STATUS_CHOICES,
+                              default='active', max_length=10)
 
     def __str__(self):
         return str(self.name)
