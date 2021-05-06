dc341f6ba97ed75b26f0f0d1d769515c6f43bbf4
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-05-06 11:26:12 +0430

    update for create forms to login and signup

---
+++ accounts/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(max_length=100)
+
+
+class SignupForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(max_length=100)
+    password_confirmation = forms.CharField(max_length=100)
